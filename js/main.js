42975b963d026e44f25cb137fe1bdaad03b2a3d2
oisa <imucup@gmail.com>
2021-01-10 15:21:30 +1100

    Update logic to include option of 3 charts general tidy up.

--- js/main.js
+++ js/main.js
@@ -1,64 +1,70 @@
-let input;
+////////////////////////////// THE VARIABLES ///////////////////////////////////
 
-const determineGraph = function(metric, colUp, colDown) {
+let input;
+let chartId;
+let chartsOnBoard = 1;
+
+////////////////////////////// THE FUNCTIONS ///////////////////////////////////
+
+const determineGraph = function(chart, metric, colUp, colDown) {
 
   if (input > 5) {
 
     if (input === 6) {
-    $(`.${colUp}`).find(`.${metric}`).attr('style','height: 25%');
+    $(`#${chart} .${colUp}`).find(`.${metric}`).attr('style','height: 25%');
     }
 
     if (input === 7) {
-    $(`.${colUp}`).find(`.${metric}`).attr('style','height: 50%');
+    $(`#${chart} .${colUp}`).find(`.${metric}`).attr('style','height: 50%');
     }
 
     if (input === 8) {
-    $(`.${colUp}`).find(`.${metric}`).attr('style','height: 75%');
+    $(`#${chart} .${colUp}`).find(`.${metric}`).attr('style','height: 75%');
     }
 
     if (input === 9) {
-    $(`.${colUp}`).find(`.${metric}`).attr('style','height: 100%');
+    $(`#${chart} .${colUp}`).find(`.${metric}`).attr('style','height: 100%');
     }
 
 // Hide upward score column and show downward arrow if opposing direction selected
-    $(`.${colUp}`).find('.arrow-up').attr('style','display: block;');
-    $(`.${colDown}`).find(`.${metric}`).attr('style','height: 0%');
-    $(`.${colDown}`).find('.arrow-down').attr('style','display: none;');
+    $(`#${chart} .${colUp}`).find('.arrow-up').attr('style','display: block;');
+    $(`#${chart} .${colDown}`).find(`.${metric}`).attr('style','height: 0%');
+    $(`#${chart} .${colDown}`).find('.arrow-down').attr('style','display: none;');
 
   }
 
   if (input < 5) {
 
     if (input === 4) {
-    $(`.${colDown}`).find(`.${metric}`).attr('style','height: 25%');
+    $(`#${chart} .${colDown}`).find(`.${metric}`).attr('style','height: 25%');
     }
 
     if (input === 3) {
-    $(`.${colDown}`).find(`.${metric}`).attr('style','height: 50%');
+    $(`#${chart} .${colDown}`).find(`.${metric}`).attr('style','height: 50%');
     }
 
     if (input === 2) {
-    $(`.${colDown}`).find(`.${metric}`).attr('style','height: 75%');
+    $(`#${chart} .${colDown}`).find(`.${metric}`).attr('style','height: 75%');
     }
 
     if (input === 1) {
-    $(`.${colDown}`).find(`.${metric}`).attr('style','height: 100%');
+    $(`#${chart} .${colDown}`).find(`.${metric}`).attr('style','height: 100%');
     }
 
 // Hide upward score column and show downward arrow if opposing direction selected
-    $(`.${colDown}`).find('.arrow-down').attr('style','display: block;');
-    $(`.${colUp}`).find(`.${metric}`).attr('style','height: 0%');
-    $(`.${colUp}`).find('.arrow-up').attr('style','display: none;');
+    $(`#${chart} .${colDown}`).find('.arrow-down').attr('style','display: block;');
+    $(`#${chart} .${colUp}`).find(`.${metric}`).attr('style','height: 0%');
+    $(`#${chart} .${colUp}`).find('.arrow-up').attr('style','display: none;');
 
   }
 
   if (input === 5) {
 
-    $(`.${colUp}`).find(`.${metric}`).attr('style','height: 10%');
-    $(`.${colDown}`).find(`.${metric}`).attr('style','height: 10%');
+    $(`#${chart} .${colUp}`).find(`.${metric}`).attr('style','height: 10%');
+    $(`#${chart} .${colDown}`).find(`.${metric}`).attr('style','height: 10%');
 
-    $(`.${colUp}`).find('.arrow-up').attr('style','display: block;');
-    $(`.${colDown}`).find('.arrow-down').attr('style','display: block;');
+    $(`#${chart} .${colUp}`).find('.arrow-up').attr('style','display: block;');
+    $(`#${chart} .${colDown}`).find('.arrow-down').attr('style','display: block;');
 
   }
 
@@ -66,50 +72,70 @@ const determineGraph = function(metric, colUp, colDown) {
 
 const resetChart = function() {
 
-  $('.verify-sc').attr('style','height: 100%');
-  $('.authenticate-sc').attr('style','height: 100%');
-  $('.complete-sc').attr('style','height: 100%');
-  $('.improvise-sc').attr('style','height: 100%');
+  $('.verify-sc, .authenticate-sc, .complete-sc, .improvise-sc').attr('style','height: 5%');
 
   $('select').val('start');
 
   $('.col').find('.arrow-up').attr('style','display: block;');
   $('.col').find('.arrow-down').attr('style','display: block;');
 
-}
+  $('#i2').hide();
+  $('#i3').hide();
+
+  chartsOnBoard = 1;
+
+};
+
+////////////////////////////// THE BUSINESS ////////////////////////////////////
 
 $('.verify').find('select').change(function() {
 
+  chartId = $(this).parent().parent().parent().attr('id');
   input = Number($(this).find('option:selected').text());
-
-  determineGraph('verify-sc', '1', '5');
+  determineGraph(chartId, 'verify-sc', '1', '5');
 
 });
 
 $('.authenticate').find('select').change(function() {
 
+  chartId = $(this).parent().parent().parent().attr('id');
   input = Number($(this).find('option:selected').text());
-
-  determineGraph('authenticate-sc', '2', '6');
+  determineGraph(chartId, 'authenticate-sc', '2', '6');
 
 });
 
 $('.complete').find('select').change(function() {
 
+  chartId = $(this).parent().parent().parent().attr('id');
   input = Number($(this).find('option:selected').text());
-
-  determineGraph('complete-sc', '3', '7');
+  determineGraph(chartId, 'complete-sc', '3', '7');
 
 });
 
 $('.improvise').find('select').change(function() {
 
+  chartId = $(this).parent().parent().parent().attr('id');
   input = Number($(this).find('option:selected').text());
-
-  determineGraph('improvise-sc', '4', '8');
+  determineGraph(chartId, 'improvise-sc', '4', '8');
 
 });
 
+$('#i2').hide();
+$('#i3').hide();
+
+$('#create').click( function() {
+
+  if (chartsOnBoard === 1) {
+    $('#i2').show();
+    chartsOnBoard++;
+    return;
+  }
+
+  if (chartsOnBoard >= 2) {
+    $('#i3').show();
+  }
+
+});
 
 
 $('#reset').click(function(){
